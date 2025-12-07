9a97a8b9f00bc7d91e61436a68566a94c0f2c6fa
KimJayhyun <dnlwjem000@gmail.com>
2025-12-07 15:27:04 +0900

    [End] Open-Closed Principle

--- src/main/java/com/company/openclose/Subscriber.java
+++ src/main/java/com/company/openclose/Subscriber.java
@@ -1,6 +1,47 @@
 package com.company.openclose;
 
 
-public class Subscriber {
+// base class - closed for modification
+public abstract class Subscriber {
+    protected Long subscriberId;
 
+    protected String address;
+
+    protected Long phoneNumber;
+
+    protected int baseRate;
+
+    public Long getSubscriberId() {
+        return this.subscriberId;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBaseRate() {
+        return this.baseRate;
+    }
+
+    public void setBaseRate(int baseRate) {
+        this.baseRate = baseRate;
+    }
+
+    public abstract double calculateBill(); // extension
 }
